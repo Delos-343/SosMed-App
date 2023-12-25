1a7e17aa05cf6295b3cf6cfd9b8c21b2477aecf2
Delos-343 <mfachry.handoko@gmail.com>
2023-12-25 13:17:31 +0800

    Created Btn component

---
+++ lib/components/button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+
+  final Function onTap;
+  final String text;
+
+  const MyButton({
+    super.key,
+
+    required this.onTap,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[700],
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
