9515208287cad9d69511cc1bdc8163d1d6452fe5
Delos-343 <mfachry.handoko@gmail.com>
2023-12-25 12:35:20 +0800

    setup login page layout

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,6 +5,42 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+          
+              SizedBox(height: 50),
+          
+              // Logo
+              Icon(
+                Icons.lock,
+                size: 100,
+              ),
+          
+              SizedBox(height: 50),
+          
+              // Welcome Back Msg
+              Text(
+                'Welcome Back, broski!'
+              ),
+          
+              SizedBox(height: 25),
+          
+              // Email
+          
+              // Password
+          
+              // Sign In Btn
+          
+              // Register Btn
+          
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
