625b8393c235b0283a0113081dd9d8692245429e
Delos-343 <mfachry.handoko@gmail.com>
2023-12-25 12:55:35 +0800

    Fixed error in styling

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -13,7 +13,6 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
 
   final emailTextController = TextEditingController();
-
   final passwordTextController = TextEditingController();
 
   @override
