651d0ec9441d00a897c988787a3187566c43b95c
Delos-343 <mfachry.handoko@gmail.com>
2023-12-29 19:01:20 +0800

    Setup auth

--- lib/auth/auth.dart
+++ lib/auth/auth.dart
@@ -1,11 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:sosmed_app/auth/login_or_register.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if(snapshot.hasData) {
+            return const HomePage();
+          } else {
+            return const LoginOrRegister();
+          }
+        }
+      ),
+    );
   }
 }
